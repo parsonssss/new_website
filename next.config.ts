3bd390deeeea6e46e70c5be2ad1e8326f9fd7ea1
parsonssss <34808813+parsonssss@users.noreply.github.com>
2025-05-14 20:16:47 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // 构建时忽略ESLint错误
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
